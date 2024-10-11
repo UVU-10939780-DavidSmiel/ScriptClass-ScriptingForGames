efec7caac71629b0b4164d2f2708a642f775c1d7
UVU-10939780-DavidSmiel <10939780@uvu.edu>
2024-10-10 18:00:25 -0600

    lots of things including interactiona dn degubloigns

---
+++ AdventureGame/Assets/Scripts/SimpleTriggerEventBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleTriggerEventBehaviour : MonoBehaviour
+{
+    
+    public UnityEvent triggerEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+        Debug.Log("Ow");
+    }
+}
